58860a1257176d0396500c1ebf032aeb8d9a04f3
Reed Stuhlreyer <reedstuhlreyer@Reeds-MacBook-Air.local>
2025-12-02 22:33:14 -0500

    updates made to work section

--- src/sections/Skills.vue
+++ src/sections/Skills.vue
@@ -3,10 +3,17 @@
     <div class="text-center max-w-xl">
       <h2 class="text-4xl font-semibold mb-4">Skills</h2>
       <div class="flex flex-wrap justify-center gap-3 text-lg">
-        <span class="px-4 py-2 border rounded-lg">Vue.js</span>
         <span class="px-4 py-2 border rounded-lg">Python</span>
+        <span class="px-4 py-2 border rounded-lg">Version Control</span>
+        <span class="px-4 py-2 border rounded-lg">CI/CD</span>
         <span class="px-4 py-2 border rounded-lg">AI/ML</span>
-        <span class="px-4 py-2 border rounded-lg">Node.js</span>
+        <span class="px-4 py-2 border rounded-lg">Java</span>
+        <span class="px-4 py-2 border rounded-lg">C#</span>
+        <span class="px-4 py-2 border rounded-lg">Enterpirse Integrations</span>
+        <span class="px-4 py-2 border rounded-lg">Rest APIs</span>
+        <span class="px-4 py-2 border rounded-lg">Web Services</span>
+        <span class="px-4 py-2 border rounded-lg">Salesforce</span>
+        <span class="px-4 py-2 border rounded-lg">Mulesoft</span>
       </div>
     </div>
   </section>
